1e55cf9320f40678043ac30cbc2bd86bcc9036bb
Phil Hartley <philh725@gmail.com>
2018-10-23 00:42:26 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,19 @@
+def input_to_index(input)
+  
+end
+
+def display_board(board)
+  
+end
+
+def valid_move?(board, index)
+  
+end
+
+def position_taken?(board, index)
+  
+end
+
+def move()
+  
+end
